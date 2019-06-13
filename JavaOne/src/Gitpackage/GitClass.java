555914fb44b77d355c4917ce2977f825960b73f3
sudha <sudhassk_04@yahoo.com>
2019-06-13 13:35:32 +0530

    The changes has to be committed

--- JavaOne/src/Gitpackage/GitClass.java
+++ JavaOne/src/Gitpackage/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("Gittesting");
         System.out.println("Successfully done");
+        System.out.println("Welcome back to Git world");
 	}
 
 }
